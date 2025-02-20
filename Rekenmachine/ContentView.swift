4418cc27285c966dd37423c152cfb1c46f6dcad7
Gyattfinder <brent.hoozee@jopie.be>
2025-02-20 10:50:03 +0100

    Initial Commit

---
+++ Rekenmachine/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Rekenmachine
+//
+//  Created by Brent Hoozee on 20/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rekenmachine/RekenmachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  RekenmachineApp.swift
+//  Rekenmachine
+//
+//  Created by Brent Hoozee on 20/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct RekenmachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
